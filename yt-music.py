ad96a7e9ef445000963187dad16b218f5afb3dbd
1SlenderGaming1 <39316039+1SlenderGaming1@users.noreply.github.com>
2023-10-07 01:49:57 -0400

    implement parse_tag()

--- yt-music.py
+++ yt-music.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 
 # NOTE: possibly use https://pypi.org/project/rich-argparse/
+# TODO: turn every audio file's artist tag into a proper array & make a PR for it    
 
+# metadata in 
+# https://github.com/yt-dlp/yt-dlp#modifying-metadata
+
+from glob import iglob
+from pprint import pprint
 import subprocess, argparse
 from mutagen._file import File, FileType as AudioFile
 
-__version__ = 0.1
-attributes = ( 'album', 'copyright', 'encodedby', 'title', 'artist', 'albumartist', 'discnumber', 'tracknumber', 'genre', 'date' )
+__version__ = 0.2
+attributes = ( 
+            'album', 
+            'albumartist', 
+            'artist',
+            'copyright', 
+            'date',
+            'discnumber', 
+            'encodedby', 
+            'genre', 
+            'title', 
+            'tracknumber', 
+)
 # TODO: redo all of this shit...
 
 def get_args() -> argparse.Namespace:
-    # don't get any funny ideas to map through a list to get these
+    # NOTE: don't get any funny ideas to map through a list to get these
+    # NOTE: date excluded on purpose
     parser = argparse.ArgumentParser(prog='yt-music',
         description="Downloads YouTube videos as music with overwritable metadata.")
     parser.add_argument('links', nargs="+",
@@ -20,26 +38,53 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('--albumartist') # --Aa misbehaved in help
     parser.add_argument('-c', '--copyright')
     parser.add_argument('-d', '--description')
-    parser.add_argument('-D', '--discnumber', type=int)
+    parser.add_argument('-D', '--discnumber')
     parser.add_argument('-e', '--encodedby')
     parser.add_argument('-g', '--genre',)
-    parser.add_argument('-t', '--title', dest='title')
-    parser.add_argument('-T', '--tracknumber', dest='tracknumber')
+    parser.add_argument('-t', '--title')
+    parser.add_argument('-T', '--tracknumber')
     parser.add_argument('-v', '--version', action='version',
                     version=f"%(prog)s {__version__}")
     return parser.parse_args()
 
-def parse_artists(artists: list[str]):
-    artists.sort()
-    return ' / '.join(artists)
+def parse_tag(tag:str, args:argparse.Namespace, file:AudioFile) -> list[str]|None:
+    if (a_tag := vars(args).get(tag)) is not None:
+        return a_tag
+    
+    if (f_tag := file.get(tag)) is not None:
+        if tag == 'date':   # YYYY-MM-DD format
+            try:
+                temp = str(int(f_tag[0]))      # type:ignore
+                return [f'{temp[:4]}-{temp[4:6]}-{temp[6:]}']
+            except (ValueError, TypeError): 
+                pass
+        return f_tag
 
-def parse_to_dict(f:AudioFile) -> dict: 
+    return []
+    
+
+
+
+def save_meta(args:argparse.Namespace, file:AudioFile) -> dict: 
     return {}
 
-
 if __name__ == "__main__":
-    f:AudioFile = File('./samples/sample.mp3', easy=True) # type:ignore
-    f.update({ 'artist':'TEST_ARTIST/TEST_ARTIST2' })
-    f.save()
+    args =  get_args()
 
-    # print(f.tags['artist'])
+    for f in iglob("./samples/*"):
+        file:AudioFile = File(f, easy=True)    # type:ignore
+
+        print(f)
+        print("-"*len(f))
+        for i in attributes:
+            print(i + '\t\t' + str(parse_tag(i, args, file)))
+        
+        # a_keys = set(vars(args).keys())
+        # f_keys = set(file.keys())
+        #
+        # # pprint(save_meta(args, File(f, easy=True)))
+        # pprint(a_keys.intersection(f_keys))
+        print()
+        
+
+
