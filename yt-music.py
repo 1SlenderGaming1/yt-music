65c7e7e66f7c9d505ea51d31c3e2a53d9c4b1c62
1SlenderGaming1 <39316039+1SlenderGaming1@users.noreply.github.com>
2023-10-23 01:37:24 -0400

    download_urls

--- yt-music.py
+++ yt-music.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # NOTE: possibly use https://pypi.org/project/rich-argparse/
 # TODO: turn every audio file's artist tag into a proper array & make a PR for it    
 
 # metadata in 
 # https://github.com/yt-dlp/yt-dlp#modifying-metadata
-
 from glob import iglob
-from pprint import pprint
-import subprocess, argparse
+from typing import Iterator
+import requests, argparse
+from subprocess import check_call, check_output, getoutput
 from mutagen._file import File, FileType as AudioFile
 from mutagen._util import MutagenError
-import os
+
 
 __version__ = 0.4
 attributes = ( 
@@ -26,6 +26,7 @@ attributes = (
             'title', 
             'tracknumber'
 )
+
 
 def get_args() -> argparse.Namespace:
     # NOTE: don't get any funny ideas to map through a list to get these
@@ -65,7 +66,7 @@ def parse_tag(tag:str, args:argparse.Namespace, file:AudioFile) -> list[str]|Non
         return f_tag
 
     return None
-    
+
 def save_meta(args:argparse.Namespace, file:AudioFile): 
     # parse tags and strip of empty values
     d = { tag : parsed
@@ -79,31 +80,53 @@ def save_meta(args:argparse.Namespace, file:AudioFile):
         file.save()
     except MutagenError:
         raise ValueError("File couldn't be saved")
+
+def download_urls(args:argparse.Namespace) -> tuple[AudioFile,...]:
+    links:list[str]   = []
+    skipped:list[str] = []
+
+    for l in args.links:
+        try:
+            url = l if l.startswith('https://') else f'https://{l}'
+            links.append(requests.get(url).url)
+        except requests.ConnectionError:
+            skipped.append(l)
     
-def download_urls(args:argparse.Namespace) -> list[str]:
-    if 0 != subprocess.call((
-            'yt-dlp',
-            '-x', '-q', '--progress',
+    if len(skipped) > 0:
+        print("Skipping")
+        print("\n\t".join(skipped))
+    
+    try:
+        path = p+'/' if (p:=args.path.lstrip().rstrip()) else ''
+        yt_dlp = ('yt-dlp', '-o', f'{path}%(title)s.opus')
+
+        check_call((
+            *yt_dlp,
+            '-x',
+            '--audio-format', 'opus',
+            '--quiet',
+            '--progress',
             '--embed-metadata',
             '--embed-thumbnail',
-            '-o', f'{args.path}/%(title)s.%(ext)s',
-            " ".join(args.links)),
-        # stdout=subprocess.DEVNULL,
-        # stderr=subprocess.STDOUT
-    ):
-        pass
-    
+            *links))
+
+        # get titles from links
+        raw_names = check_output((*yt_dlp, '--print', 'filename', *links))
+        names = tuple(i.replace(r'b"', '') for i in str(raw_names).split(r'\n')[:-1])
+
+        return tuple(File(n, easy=True) for n in names)
+
+
+        return tuple(map(File, names))
+    except ChildProcessError as e:
+        raise ValueError("Links", links, "caused the error", e)
+
 
 if __name__ == "__main__":
     args =  get_args()
-    # print(*download_urls(args),sep='\n')
 
-    path:str = p if (p:=args.path) else '.'
-    print(*os.walk(path))
-
-
-
-
+    __import__('pprint').pprint(download_urls(args))
+    # __import__('pprint').pprint(download_paths(args.path, args.links))
 
 
 
