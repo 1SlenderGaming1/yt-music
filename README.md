aaf9c2c7adb03b3919fd24ecff7f797aac94e292
1SlenderGaming1 <39316039+1SlenderGaming1@users.noreply.github.com>
2023-11-19 21:26:46 -0500

    final commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 yt-dlp wrapper that downloads music and adds metadata
 
 This project exists for personal usage. Fork it for yourself if you want to improve upon it I guess.
+
+## **Abandoned Due to metadata and thumbnail frustrations**
+Every time I changed the metadata the thumbnail vanished and I've been working on this for far too long. There's better things to do than mash your head against a wall for months.
+
--- yt-music.py
+++ yt-music.py
@@ -1,134 +1,151 @@
-#!/usr/bin/python
-
-# NOTE: possibly use https://pypi.org/project/rich-argparse/
-# TODO: turn every audio file's artist tag into a proper array & make a PR for it    
-
-# metadata in 
-# https://github.com/yt-dlp/yt-dlp#modifying-metadata
+#!/usr/bin/env python
+from argparse import ArgumentParser, Namespace
+from collections.abc import Generator
 from glob import iglob
-from typing import Iterator
-import requests, argparse
-from subprocess import check_call, check_output, getoutput
-from mutagen._file import File, FileType as AudioFile
+from pprint import pprint
+
 from mutagen._util import MutagenError
+from mutagen.oggopus import Open as OggOpen
+from pytube import Playlist, YouTube
+from pytube.exceptions import RegexMatchError
+import yt_dlp
+
+__version__ = 0.7
+
+type Options = dict
+
+current_filename = ""
 
 
-__version__ = 0.4
-attributes = ( 
-            'album', 
-            'albumartist', 
-            'artist',
-            'copyright', 
-            'date',
-            'discnumber', 
-            'encodedby', 
-            'genre', 
-            'title', 
-            'tracknumber'
-)
+def main():
+    args =  get_args()
+    OPTS = {
+        "quiet": args.quiet,
+        'overwrites':True,
+        'outtmpl':{ 'default':f'{args.path}%(title)s.opus' },
+        'format':'bestaudio/best',
+        'postprocessors':[
+            { 'key':'FFmpegExtractAudio', 'preferredcodec':'opus' },
+            { 'key':'EmbedThumbnail' },
+        ]
+    }
+    
+    # https://www.youtube.com/watch?v=tu4OQ8LQn4w&list=OLAK5uy_kmYYWT5HDnLkg0Jo_CepBLnZc7DuR5OiM
 
 
-def get_args() -> argparse.Namespace:
-    # NOTE: don't get any funny ideas to map through a list to get these
-    # NOTE: date excluded on purpose
-    parser = argparse.ArgumentParser(prog='yt-music',
+    for filepath, link, playlist in download_audio(OPTS, args):
+        parse_metadata(link, args, playlist)
+
+
+
+
+def get_args() -> Namespace:
+    parser = ArgumentParser(prog='yt-music',
         description="Downloads YouTube videos as music with overwritable metadata.")
     parser.add_argument('links', nargs="*",default="", type=str,
                         help='Space-separated links to download')
     parser.add_argument('-v', '--version', action='version',
                         version=f"%(prog)s {__version__}")
-    parser.add_argument('-p', '--path', default="temp") 
-    parser.add_argument('-a', '--artist', action='append', metavar='ARTIST(S)')
+    parser.add_argument('-q', '--quiet', action="store_true")
+    parser.add_argument('-p', '--path', default="./", type=dir_path) 
+
     parser.add_argument('-A', '--album')
-    parser.add_argument('--albumartist') # --Aa misbehaved in help
-    parser.add_argument('-c', '--copyright')
-    parser.add_argument('-d', '--description')
-    parser.add_argument('-D', '--discnumber')
-    parser.add_argument('-e', '--encodedby')
-    parser.add_argument('-g', '--genre',)
+    parser.add_argument('-l', '--albumartist')
+    parser.add_argument('-a', '--artist', action='append', metavar='ARTIST(S)')
+    parser.add_argument('-g', '--genre')
     parser.add_argument('-t', '--title')
-    parser.add_argument('-T', '--tracknumber')
     return parser.parse_args()
 
-def parse_tag(tag:str, args:argparse.Namespace, file:AudioFile) -> list[str]|None:
-    if (a_tag := vars(args).get(tag)) is not None:
-        if type(a_tag) != list:
-            return [a_tag]
-        return a_tag
+def save_meta(filepath:str, metadata:dict, args):
+    file = OggOpen(filepath)
+    link=''
+    # parse_metadata(link, args, )
     
-    if (f_tag := file.get(tag)) is not None:
-        if tag == 'date':   # YYYY-MM-DD format
-            try:
-                temp = str(int(f_tag[0]))      # type:ignore
-                return [f'{temp[:4]}-{temp[4:6]}-{temp[6:]}']
-            except (ValueError, TypeError): 
-                pass
-        return f_tag
+    # if file is None:
+    #     raise ValueError(f"{filepath} was not found.")
 
-    return None
+    # file
 
-def save_meta(args:argparse.Namespace, file:AudioFile): 
-    # parse tags and strip of empty values
-    d = { tag : parsed
-        for tag in attributes
-        if (parsed := parse_tag(tag, args, file)) is not None
+    pass
+
+def parse_metadata(link:str, args:Namespace, 
+                   playlist: Playlist|None = None,
+                   index:int|None = None) -> dict[str,str]:
+    """Parse and merge metadata from arguments and video link into a single dict.
+
+    - link: video link
+    - args: user arguments
+    - playlist: playlist object
+    - index: current position of playlist, pass via enumerate()
+    """
+
+    vid = YouTube(link)
+
+    if playlist is None and 'list=' in link:
+        playlist = Playlist("https://youtube.com/playlist?list="+link.split("list=")[1])
+    
+    d = {
+        'artist':vid.author,
+        'albumartist': vid.author,
+        'comment': f"{vid.watch_url}{_des+'\n' if ( _des:=vid.description) else ''}",
+        'date': vid.publish_date.strftime('%Y-%m-%d'), # type:ignore
+        'title': vid.title,
     }
 
-    file.update(d)
-    
-    try:
-        file.save()
-    except MutagenError:
-        raise ValueError("File couldn't be saved")
+    if playlist is not None:
+        d['tracknumber'] = f"{index}/{playlist.length}"
+        d['albumartist'] = vid.author,
+        d['album'] = playlist.title
 
-def download_urls(args:argparse.Namespace) -> tuple[AudioFile,...]:
-    links:list[str]   = []
-    skipped:list[str] = []
+    overwrite = { k:v for k,v in {
+        'album': args.album,
+        'albumartist': args.albumartist,
+        'artist': args.artist,
+        'genre': args.genre,
+        'title': args.title,
+        }.items() if v is not None }
 
-    for l in args.links:
-        try:
-            url = l if l.startswith('https://') else f'https://{l}'
-            links.append(requests.get(url).url)
-        except requests.ConnectionError:
-            skipped.append(l)
-    
-    if len(skipped) > 0:
-        print("Skipping")
-        print("\n\t".join(skipped))
-    
-    try:
-        path = p+'/' if (p:=args.path.lstrip().rstrip()) else ''
-        yt_dlp = ('yt-dlp', '-o', f'{path}%(title)s.opus')
+    return {**d, **overwrite}
 
-        check_call((
-            *yt_dlp,
-            '-x',
-            '--audio-format', 'opus',
-            '--quiet',
-            '--progress',
-            '--embed-metadata',
-            '--embed-thumbnail',
-            *links))
+def download_audio(opts: dict, args:Namespace) -> Generator[tuple[str, str, Playlist|None], None, None]:
+    """
+    Downloads videos and applies thumbnails.
+    Returns expected filepaths.
+    """
 
-        # get titles from links
-        raw_names = check_output((*yt_dlp, '--print', 'filename', *links))
-        names = tuple(i.replace(r'b"', '') for i in str(raw_names).split(r'\n')[:-1])
+    links:list[str] = args.links   
 
-        return tuple(File(n, easy=True) for n in names)
+    with yt_dlp.YoutubeDL(opts) as yt:
+        for link in links:
+            if "playlist?list=" in link:
+                try:
+                    links.extend(Playlist(link))
+                    print("Appending playlist to download queue")
+                    continue
+                except RegexMatchError:
+                    print("Skipping ", link)
+                    continue
 
+            # validate and strip link
+            try:
+                l = YouTube(link).watch_url
+            except RegexMatchError:
+                print('Skipping ', link)
+                continue
 
-        return tuple(map(File, names))
-    except ChildProcessError as e:
-        raise ValueError("Links", links, "caused the error", e)
+            info:dict    = yt.extract_info(l, download=True) # type:ignore
+            filename:str = info['entries'][0]['requested_downloads'][0]['filename']
 
+            try:
+                playlist = Playlist(link)
+            except RegexMatchError:
+                playlist=None
+
+            yield filename, l, playlist
+    return None
+
+def dir_path(p:str) -> str:
+    return p if p.endswith("/") else p + "/"
 
 if __name__ == "__main__":
-    args =  get_args()
-
-    __import__('pprint').pprint(download_urls(args))
-    # __import__('pprint').pprint(download_paths(args.path, args.links))
-
-
-
-
-
+    main()
